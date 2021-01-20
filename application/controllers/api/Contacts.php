f71a4440efd5bb4133a2fe132c8b3d2d41c04cb9
Shivam Gautam <shivamgautam@archiztechnologies.com>
2021-01-20 18:13:59 +0530

    contact api

---
+++ application/controllers/api/Contacts.php
@@ -0,0 +1,263 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+class Contacts extends REST_Controller {
+  function __construct() 
+  {
+      parent::__construct();
+      $this->load->library('form_validation');
+	  $this->load->model(array('enquiry_model','common_model','client_model'));
+  }
+
+  	public function contacts_list_page_post()
+    {
+      $user_id= $this->input->post('user_id');
+      $company_id = $this->input->post('company_id');
+      $offset = $this->input->post('offset')??0;
+      $limit = $this->input->post('limit')??10;
+
+       $res= array();
+    
+        $total = $this->client_model->getContactList(array(),'count',$company_id,$user_id);
+
+        $data['result'] = $this->client_model->getContactList(array(),'data',$company_id,$user_id,$limit,$offset);
+                  
+          if(!empty($data['result']->result()))
+          {
+            $res= array();
+            
+            $res['offset'] = $offset;
+            $res['limit'] = $limit;
+            $res['total'] = $total;
+            $res['list'] = $data['result']->result();
+
+            $this->set_response([
+                'status' => TRUE,
+                'data' =>$res
+                 ], REST_Controller::HTTP_OK);
+          }   
+		else
+         {
+	    
+	        $this->set_response([
+	          'status' => false,
+	          'msg' =>'not found'
+	          ], REST_Controller::HTTP_OK);
+	      }
+    }
+
+
+    public function contact_details_post()
+    {
+    	$id = $this->input->post('contact_id');
+
+    	$value = $this->db->where('cc_id',$id)->get('tbl_client_contacts')->row();
+
+    	if(!empty($value))
+    	{
+    		 $this->set_response([
+                'status' => TRUE,
+                'data' =>$value
+                 ], REST_Controller::HTTP_OK);
+    	}
+    	else
+    	{
+    		 $this->set_response([
+	          'status' => false,
+	          'msg' =>'not found'
+	          ], REST_Controller::HTTP_OK);
+    	}
+    }
+
+    public function delete_contact_post()
+    {
+    	$cc_id = $this->input->post('contact_id');
+    	$comp_id = $this->input->post('company_id');
+    	$enquiry_id = $this->input->post('enquiry_id');
+    	$user_id = $this->input->post('user_id');
+    	$this->form_validation->set_rules('contact_id','contact_id','required|trim');
+    	$this->form_validation->set_rules('company_id','company_id','required|trim');
+    	$this->form_validation->set_rules('enquiry_id','enquiry_id','required|trim');
+    	$this->form_validation->set_rules('user_id','user_id','required|trim');
+
+    	if($this->form_validation->run()==true)
+    	{
+    		$this->db->where(array('cc_id'=>$cc_id,'comp_id'=>$comp_id,'client_id'=>$enquiry_id));
+    		$this->db->delete('tbl_client_contacts');
+
+    		if($this->db->affected_rows())
+    		{
+          $res=  $this->db->select('Enquery_id')->where('enquiry_id',$enquiry_id)->get('enquiry')->row();
+          if(!empty($res))
+          {
+          $this->load->model('Leads_Model');
+          $this->Leads_Model->add_comment_for_events('Contact Deleted.',$res->Enquery_id,0,$user_id);
+          }
+
+
+    			$this->set_response([
+                  'status' => true,
+                  'message' =>'Deleted Successfully.',
+               ], REST_Controller::HTTP_OK);
+    		}
+    		else
+    		{
+    			$this->set_response([
+                  'status' => false,
+                  'message' =>'Visit Not found',
+               ], REST_Controller::HTTP_OK);
+    		}
+    	}
+  		else 
+        {		     
+  		     $this->set_response([
+                  'status' => false,
+                  'message' =>strip_tags(validation_errors())
+               ], REST_Controller::HTTP_OK);
+  		  }
+
+    }
+
+    public function save_contact_post()
+    {
+    	$cc_id = $this->input->post('contact_id');
+    	$comp_id = $this->input->post('company_id');
+    	$enquiry_id = $this->input->post('enquiry_id');
+    	$user_id = $this->input->post('user_id');
+
+      $name = $this->input->post('name');
+      $mobile = $this->input->post('mobileno');
+      $email = $this->input->post('email');
+      $otherdetails = $this->input->post('otherdetails');
+
+    	$this->form_validation->set_rules('company_id','company_id','required|trim');
+    	$this->form_validation->set_rules('enquiry_id','enquiry_id','required|trim');
+    	$this->form_validation->set_rules('user_id','user_id','required|trim');
+
+    	if($this->form_validation->run()==true)
+    	{
+    		$this->load->model(array('Client_Model','Enquiry_model','Leads_Model'));
+
+    		$data = array(
+                'c_name' => $name,
+                'emailid' => $email,
+                'contact_number' => $mobile,
+                'designation' => $this->input->post('designation'),
+                'other_detail' => $otherdetails,
+                'decision_maker' => $this->input->post('decision_maker')??0,
+            );
+    		$done = 0;
+            $res = $this->db->where(array('enquiry_id'=>$enquiry_id))->get('enquiry')->row();
+            if(!empty($res))
+            {	
+            	if(!empty($cc_id))
+	            {
+	            	$this->db->where('cc_id',$cc_id)->update('tbl_client_contacts',$data);
+	            	$this->Leads_Model->add_comment_for_events('Contact Updated',$res->Enquery_id,0,$user_id);
+	            }
+	            else
+	            {	
+                $data['comp_id'] = $comp_id;
+                $data['client_id'] = $enquiry_id;
+                $this->db->insert('tbl_client_contacts',$data);
+	            	$this->Leads_Model->add_comment_for_events('Contact Added',$res->Enquery_id,0,$user_id);
+	            }
+	            $done = 1;
+            }	
+
+            if($done)
+            {
+            	
+            	$this->set_response([
+                  'status' => true,
+                  'message' =>'Saved Successfully.',
+               ], REST_Controller::HTTP_OK);
+			       }
+            else
+            {
+				$this->set_response([
+                  'status' => FALSE,
+                  'message' =>'Unable to Save.',
+               ], REST_Controller::HTTP_OK);
+            }
+    	}
+  		else 
+        {		     
+  		     $this->set_response([
+                  'status' => false,
+                  'message' =>strip_tags(validation_errors())
+               ], REST_Controller::HTTP_OK);
+  		  }
+
+    }
+
+    public function for_data_list_post()
+    {
+    	$comp_id = $this->input->post('company_id');
+    	$user_id = $this->input->post('user_id');
+    	$this->form_validation->set_rules('company_id','company_id','required|trim');
+    	$this->form_validation->set_rules('user_id','user_id','required|trim');
+        if($this->form_validation->run()==true)
+        {
+
+        	$all_reporting_ids  = $this->common_model->get_categories($user_id);
+
+	    	$this->db->select('tbl_visit.enquiry_id,CONCAT(COALESCE(enquiry.name,"")," ",COALESCE(enquiry.lastname,"")) as name');
+	        $this->db->from('tbl_visit');
+	        $this->db->join('enquiry','enquiry.enquiry_id=tbl_visit.enquiry_id','left');
+	        $this->db->where("tbl_visit.comp_id",$comp_id);
+
+	        $where="";
+	        $where .= "( enquiry.created_by IN (".implode(',', $all_reporting_ids).')';
+	        $where .= " OR enquiry.aasign_to IN (".implode(',', $all_reporting_ids).'))';
+	        $this->db->where($where);
+	        $res = $this->db->get()->result();
+
+         	if(!empty($res))
+            {
+            	
+            	$this->set_response([
+                  'status' => true,
+                  'data' =>$res,
+               ], REST_Controller::HTTP_OK);
+			}
+            else
+            {
+				$this->set_response([
+                  'status' => FALSE,
+                  'message' =>'No data.',
+               ], REST_Controller::HTTP_OK);
+            }
+    	}
+  		else 
+        {		     
+  		     $this->set_response([
+                  'status' => false,
+                  'message' =>strip_tags(validation_errors())
+               ], REST_Controller::HTTP_OK);
+  		}
+    }
+
+    public function rating_list_post()
+    {
+    	$comp_id = $this->input->post('company_id');
+    	$this->form_validation->set_rules('company_id','company_id','required|trim');
+    	if($this->form_validation->run()==true)
+    	{
+    		$data = array('1 Star','2 Star','3 Star','4 Star','5 Star');
+    		$this->set_response([
+                  'status' => true,
+                  'data' =>$data,
+               ], REST_Controller::HTTP_OK);
+    	}
+    	else
+    	{
+    		$this->set_response([
+                  'status' => false,
+                  'message' =>strip_tags(validation_errors())
+               ], REST_Controller::HTTP_OK);
+    	}
+    }
+}
