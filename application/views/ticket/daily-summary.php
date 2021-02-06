a0e432ff5fa40a8f95d2f90a92558c7d6e877ae4
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2021-02-05 22:05:54 -0500

    test

--- application/views/ticket/daily-summary.php
+++ application/views/ticket/daily-summary.php
@@ -1,25 +1,25 @@
 <?php
 $comp_id = 65;
 $this->db->where('comp_id',$comp_id);
+//$this->db->limit(10);
 $failurePoints = $this->db->get('tbl_ticket_subject')->result_array();
 $res = array();
 
 if(!empty($failurePoints)){
     foreach($failurePoints as $key =>$value){
         $this->db->select('count(tbl_ticket.ticket_substage) as c,lead_description.description');
-        $this->db->from('lead_description');
-        $this->db->where('tbl_ticket.category',$value['id']);
-        $this->db->join('tbl_ticket','tbl_ticket.ticket_substage=lead_description.id','left');
-        $this->db->group_by('tbl_ticket.ticket_substage');   
-        $result    =   $this->db->get();
-
+        $this->db->from('lead_description');        
+        $this->db->where('lead_description.comp_id',$comp_id);
+        $this->db->join('(select * from tbl_ticket where date(coml_date)='.$_GET['date'].' AND category='.$value['id'].') as tbl_ticket','tbl_ticket.ticket_substage=lead_description.id','left');
+        $this->db->group_by('lead_description.id');   
+        $result    =   $this->db->get()->result_array();
         $k = $value['subject_title'];
         $res[$k] = $result;
     }
 }
-echo "<pre>";
-print_r($res);
-echo "<pre>";
+// echo "<pre>";
+// print_r($res);
+// echo "<pre>";
 ?>
 <table class="datatable table table-striped table-bordered">
     <thead>
@@ -40,10 +40,15 @@ echo "<pre>";
     </thead>
     <tbody>
     <?php
-        if(!empty($failurePoints)){
-            foreach($failurePoints as $key=>$value){
+        if(!empty($res)){
+            foreach($res as $key=>$value){
                 echo "<tr>";
-                echo "<th>".$value['subject_title']."</th>";
+                echo "<td>".$key."</td>";
+                if(!empty($value)){
+                    foreach($value as $k =>$v){
+                        echo "<td>".$v['c']."</td>";
+                    }
+                }
                 echo "</tr>";
             }
         }
