162c9ed1cf7f5d2703236075c3e3df92340cc356
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2021-02-09 14:13:18 -0500

    test

--- application/controllers/Message.php
+++ application/controllers/Message.php
@@ -630,16 +630,16 @@ public function chat_start(){
    public function test_start(){
 	$config = Array(
 		'protocol' => 'smtp',
-		'smtp_host' => 'ssl://smtp3.netcore.co.in',
+		'smtp_host' => 'ssl://smtppro.zoho.com',
 		'smtp_port' => 465,
-		'smtp_user' => 'crm@vtransgroup.com',
-		'smtp_pass' => '@FebCrm21#',
+		'smtp_user' => 'suraj@archiztechnologies.com',
+		'smtp_pass' => 'Archiz321',
 		'mailtype'  => 'html', 
 		'charset'   => 'iso-8859-1'
 	);
 	$this->load->library('email', $config);
 
-		$this->email->from('crm@vtransgroup.com', 'ur Name');
+		$this->email->from('suraj@archiztechnologies.com', 'ur Name');
 		$this->email->to('prokanhaiya@gmail.com');
 		$this->email->subject('Email Test');
 		$this->email->message('Testing the email class.');
