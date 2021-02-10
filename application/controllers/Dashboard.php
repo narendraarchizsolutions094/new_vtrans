2799735efe4da916a9586db968123df3226b40f9
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2021-02-10 15:55:21 -0500

    test

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -144,8 +144,8 @@ class Dashboard extends CI_Controller {
         }
        }
        $this->db->select('response,id');
-       $this->db->where('s',0);
-       $this->db->limit(1000);
+       $this->db->where('s!=',3);
+       $this->db->limit(5);
        $res_fb=$this->db->get('fb_setting')->result();
         if(!empty($res_fb)){
         foreach ($res_fb as $d){
@@ -244,7 +244,7 @@ class Dashboard extends CI_Controller {
                 if ($err) {
                 } else { 
                    $this->db->set('is_status',1);
-                   $this->db->set('s',1);
+                   $this->db->set('s',3);
                    $this->db->set('r1',$response);
                    $this->db->where('id',$d->id);
                    $this->db->update('fb_setting');
