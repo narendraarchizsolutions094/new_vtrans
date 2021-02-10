43c43b8e237477399b27a77491b54c3bdac68d06
Kanhaiya <prokanhaiya@gmail.com>
2021-02-10 20:57:56 +0530

    live push

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Auth extends CI_Controller {
-	public function __construct(){
+	public function __construct(){   
 		parent::__construct();		
 		$this->lang->load("activitylogmsg","english");
 	}
