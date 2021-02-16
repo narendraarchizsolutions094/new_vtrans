d1f2a6c7a137cf00253a22bf13a2cc109b8bd90c
DESKTOP-B86V9V5\Kanhaiya <prokanhaiya@gmail.com>
2021-02-16 00:13:34 -0500

    test

--- application/controllers/Enquiry.php
+++ application/controllers/Enquiry.php
@@ -460,7 +460,11 @@ class Enquiry extends CI_Controller
         //         $this->form_validation->set_rules('mobileno', display('mobileno'), 'max_length[20]|callback_phone_check|required', array('phone_check' => 'Duplicate Entry for phone'));
         //     }
         // }
-        $this->form_validation->set_rules('mobileno', display('mobileno'), 'max_length[20]|callback_phone_check');
+        if($this->session->companey_id == 90){
+            $this->form_validation->set_rules('mobileno', display('mobileno'), 'max_length[20]|callback_phone_check');
+        }else{
+            $this->form_validation->set_rules('mobileno', display('mobileno'), 'max_length[20]|callback_phone_check', array('phone_check' => 'Duplicate Entry for phone'));
+        }
         if (!empty($this->input->post('email'))) {
             $this->form_validation->set_rules('email', display('email'), 'callback_email_check', array('email_check' => 'The Email you entered is already exist'));
         }
