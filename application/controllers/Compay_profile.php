3d98bdb60a61c620d7608288a898f7025ee90a5b
DESKTOP-663BG2P\Admin <harshit@archizsolutions.com>
2021-04-28 10:34:15 +0530

    query data

--- application/controllers/Compay_profile.php
+++ application/controllers/Compay_profile.php
@@ -7,7 +7,7 @@ class Compay_profile extends CI_Controller {
             redirect('login');
         }
     }
-public function company_upload_data($filter=array()){
+public function company_upload_data(){
 	
         $this->db->select('enquiry_id,company');
 		$this->db->from('enquiry');	
@@ -17,6 +17,12 @@ public function company_upload_data($filter=array()){
 			if (is_numeric($value->company)) {
                 
 			} else {
+				$this->db->select('id');
+		        $this->db->from('tbl_company');	
+                $this->db->where("(company_name LIKE '%".$value->company."%')", NULL, FALSE);		
+                $cid = $this->db->get()->row();
+				
+				if(empty($cid->id)){				
 				$key = array( 
                                 "company_name"  => $value->company,
                                 "process_id"   => '141',
@@ -25,6 +31,9 @@ public function company_upload_data($filter=array()){
 								
                 $this->db->insert('tbl_company',$key);
 				$last_id = $this->db->insert_id();
+				}else{
+				$last_id = $cid->id;
+				}
 				
                 $this->db->set('company',$last_id);
 				$this->db->where('enquiry_id',$value->enquiry_id);
