e2c58ef5e701403266c7ff91df2831254e28136b
DESKTOP-663BG2P\Admin <harshit@archizsolutions.com>
2021-07-13 20:18:53 +0530

    visit changes

--- application/config/config.php
+++ application/config/config.php
@@ -430,7 +430,8 @@ $config['standardize_newlines'] = FALSE;
 |          for backwards compatibility purposes!
 |
 */
-$config['global_xss_filtering'] = TRUE;
+//$config['global_xss_filtering'] = TRUE;
+$config['global_xss_filtering'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
