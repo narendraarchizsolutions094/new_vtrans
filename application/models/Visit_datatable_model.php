d1ad256bb7bdde518f629b77327a368008686f38
DESKTOP-663BG2P\Admin <harshit@archizsolutions.com>
2022-01-28 12:50:12 +0530

    visit

--- application/models/Visit_datatable_model.php
+++ application/models/Visit_datatable_model.php
@@ -119,7 +119,7 @@ class Visit_datatable_model extends CI_Model{
         $where .= " OR enquiry.aasign_to IN (".implode(',', $all_reporting_ids).'))'; */ 
 
         $where .= "( tbl_visit.user_id IN (".implode(',', $create_user_id).')';
-		$where .= " OR enquiry.created_by IN (".implode(',', $all_reporting_ids).')';
+		$where .= " AND enquiry.created_by IN (".implode(',', $all_reporting_ids).')';
         $where .= " OR enquiry.aasign_to IN (".implode(',', $all_reporting_ids).'))';		
 		
 /* $ids = array('40805', '40807', '40856');
