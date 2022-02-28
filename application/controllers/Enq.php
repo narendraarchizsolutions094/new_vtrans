277f8863a42aeee0b9cf36c79b335b0bcaec16bd
DESKTOP-663BG2P\Admin <harshit@archizsolutions.com>
2022-02-28 12:34:01 +0530

    update

--- application/controllers/Enq.php
+++ application/controllers/Enq.php
@@ -107,17 +107,15 @@ foreach($data['branch_lists'] as $branch){
 
 //UPDATE CLIENT NAME USING COMPANY ID + BRANCH ID(from table ticket)
 //$enq_data = $this->db->select('enquiry_id,company')->where('client_name','')->or_where('client_name',null)->or_where('sales_branch','')->where('sales_branch',null)->get('enquiry')->result();
-$enq_data = $this->db->select("enq.enquiry_id,enq.company")
+/* $enq_data = $this->db->select("enq.enquiry_id,enq.company")
 			->where("enq.client_name", '')
-			//->or_where("enq.client_name", null)
-			//->or_where("enq.sales_branch",'')
 			->where("enq.sales_branch",null)
 			->where("enq.company is NOT NULL", NULL, FALSE)
 			->from("enquiry enq")
 			->join("tbl_ticket", 'tbl_ticket.client=enq.enquiry_id', 'inner')
 			->get()
 			->result();
-//print_r(count($enq_data));exit;
+
 foreach($enq_data as $val){
 $brid = $this->db->select('branch_for')->where('client',$val->enquiry_id)->get('tbl_ticket')->row();
 if(!empty($brid->branch_for)){	
@@ -130,8 +128,6 @@ if(!empty($rab2)){
 }else{
 	$client_name = '';
 }
-//print_r($client_name);exit;
-
     $this->db->set('client_name',$client_name);
 	$this->db->set('sales_branch',$brid->branch_for);
 	$this->db->set('sales_region',$rab->region_id);
@@ -140,7 +136,7 @@ if(!empty($rab2)){
     $this->db->update('enquiry');
 	
 }
-}
+} */
 //END
 
 //UPDATE CLIENT NAME USING COMPANY GROUP NAME + SALES BRANCH
